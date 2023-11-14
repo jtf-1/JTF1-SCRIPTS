31fce1db4c014b8f77198481e19c406867517c0b
jtf-1-yomo <35670725+jtf-1-yomo@users.noreply.github.com>
2023-11-14 14:02:50 +0000

    Create missions.lua

---
+++ missions/missions.lua
@@ -0,0 +1,29 @@
+MISSIONLIST = {
+    {
+        missionName = "Caucasus Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_Caucasus_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_Caucasus_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_Caucasus_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_Caucasus_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+    {
+        missionName = "PG Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_PG_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_PG_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_PG_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_PG_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+    {
+        missionName = "NTTR Fun Map",
+        missionMenu = {
+            {menuText = "Day", missionFile = "JTF-1_NTTR_DAY_Fun_Map.miz"},
+            {menuText = "Day Weather", missionFile = "JTF-1_NTTR_DAY_WEATHER_Fun_Map.miz"},
+            {menuText = "Night", missionFile = "JTF-1_NTTR_NIGHT_Fun_Map.miz"},
+            {menuText = "Night Weather", missionFile = "JTF-1_NTTR_NIGHT_WEATHER_Fun_Map.miz"},
+        },
+    },
+}
